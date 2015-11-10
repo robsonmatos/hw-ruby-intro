541829f133985c49f74831de3738a893659b7232
robsonmatos <robymatos@gmail.com>
2015-11-10 19:49:38 -0400

    Finished homework

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -67,5 +67,15 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+  def initialize isbn, price
+    raise ArgumentError, "ISBN is an empty string" if isbn.empty?
+    raise ArgumentError, "Price is less than or equal to zero" if price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  def price_as_string
+    "$%.2f" % price
+  end
 end
